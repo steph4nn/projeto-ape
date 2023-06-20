908a1fafa215d0bc2981e1f280bf733285d1b6f2
steph4nn <110504296+steph4nn@users.noreply.github.com>
2023-06-20 20:45:37 -0300

    deu certo porra

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ def mapearMatriz(tabuleiro):
                 result+=1
     return result
 
-def verificarLados(tabuleiro):
-    for linha in range(ORDEM):
-        for coluna in range(ORDEM):
+def verificarLados(tabuleiro, linha, coluna):
+
             if coluna+1 < ORDEM and tabuleiro[linha][coluna+1] == 'N':
                 return False
             elif linha+1<ORDEM and tabuleiro[linha+1][coluna] == 'N':
@@ -29,15 +28,20 @@ def verificarLados(tabuleiro):
                 return True
 
 def gerarTabuleiro(tabuleiro):
+    cont_i=0
+    cont_j=0
     for linha in range(ORDEM):
         for coluna in range(ORDEM):
             if mapearMatriz(tabuleiro) <= 6:
-                if verificarLados(tabuleiro) == True:
+                if verificarLados(tabuleiro, cont_i, cont_j) == True:
                     tabuleiro[linha][coluna] = random.choice(LETRAS)
                 else:
                     tabuleiro[linha][coluna] = 'A'
             else:
                 tabuleiro[linha][coluna] = 'A'
+            cont_j+=1
+        cont_j = 0    
+        cont_i+=1
     return tabuleiro
 
 def mostrarTabuleiro(tabuleiro):
@@ -45,7 +49,6 @@ def mostrarTabuleiro(tabuleiro):
         for coluna in range(ORDEM):
             print(f'{tabuleiro[linha][coluna]:4}',end='')
         print('')
-
 
 
 gerarTabuleiro(jogadorA)
