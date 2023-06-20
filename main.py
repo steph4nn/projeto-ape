81ca65cc3ce904d85dc6a6999e4dd58608115677
steph4nn <110504296+steph4nn@users.noreply.github.com>
2023-06-20 19:40:33 -0300

    aaaaaaa

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ LETRAS = ['A','N']
 jogadorA = [[0]*ORDEM for linha in range(ORDEM)]
 
 
-
 def mapearMatriz(tabuleiro):
     result = 0
     for linha in range(ORDEM):
@@ -19,27 +18,25 @@ def verificarLados(tabuleiro):
     result = True
     for linha in range(ORDEM):
         for coluna in range(ORDEM):
-            if tabuleiro[linha][coluna+1] == 'N':
+            if coluna+1 < ORDEM and tabuleiro[linha][coluna+1] == 'N':
                 result = False
-            elif tabuleiro[linha+1][coluna] == 'N':
+            elif linha+1<ORDEM and tabuleiro[linha+1][coluna] == 'N':
                 result = False
-            elif tabuleiro[linha-1][coluna] == 'N':
+            elif linha-1 >= 0 and tabuleiro[linha-1][coluna] == 'N':
                 result = False
-            elif tabuleiro[linha][coluna-1] == 'N':
+            elif coluna-1 >= 0 and tabuleiro[linha][coluna-1] == 'N':
                 result = False
     return result
-
 
 def gerarTabuleiro(tabuleiro):
     for linha in range(ORDEM):
         for coluna in range(ORDEM):
             if mapearMatriz(tabuleiro) < 6:
-                tabuleiro[linha][coluna] = random.choice(LETRAS)
-            else:
-                tabuleiro[linha][coluna] = 'A'
+                if verificarLados(tabuleiro) == True:
+                    tabuleiro[linha][coluna] = random.choice(LETRAS)
+                else:
+                    tabuleiro[linha][coluna] = 'A'
     return tabuleiro
-
-
 
 def mostrarTabuleiro(tabuleiro):
     for linha in range(ORDEM):
