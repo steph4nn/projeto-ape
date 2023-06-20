31a510098991d21776bdb95c75be1da88fb7485d
steph4nn <110504296+steph4nn@users.noreply.github.com>
2023-06-20 19:14:45 -0300

    limite 6 navios

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ jogadorA = [[0]*ORDEM for linha in range(ORDEM)]
 
 
 
+def mapearMatriz(tabuleiro):
+    result = 0
+    for linha in range(ORDEM):
+        for coluna in range(ORDEM):
+            if tabuleiro[linha][coluna] == 'N':
+                result+=1
+    return result
+
 def gerarTabuleiro(tabuleiro):
     for linha in range(ORDEM):
         for coluna in range(ORDEM):
+            if mapearMatriz(tabuleiro) < 6:
                 tabuleiro[linha][coluna] = random.choice(LETRAS)
+            else:
+                tabuleiro[linha][coluna] = 'A'
     return tabuleiro
 
 
